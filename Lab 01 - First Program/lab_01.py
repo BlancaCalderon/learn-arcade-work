24f26ced66bea0f8ea1ebf3f9b9d07fc5a153703
BlancaCalderon <79267061+BlancaCalderon@users.noreply.github.com>
2021-02-18 20:28:29 +0100

    work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,12 @@
-Hi
+
+
+#Imprime en pantalla Hello world
+print("Hello world!")
+
+#imprime en pantalla varias frases
+print('It was a dark stormy night.')
+print('suddenly a shot rang out!')
+
+#uso escape code para double quote
+print("Hi! This is a double quote mark: \"we use it for printing.")
+print("This\nis\nmy\nsample.")
